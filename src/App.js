94b8221c2dfd0c2e1df730ae5280d6e573f5cd6c
dduck <kdh@rootechs.com>
2021-08-11 13:15:52 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '김덕현',
+  'birthday': '721222',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '이예나',
+  'birthday': '050512',
+  'gender': '여자',
+  'job': '주부'
+  },
+  {
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '홍길동',
+  'birthday': '721122',
+  'gender': '남자',
+  'job': '디자이너'
+  }
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map(c => { return (<Customer
+            key = {c.id}
+            id = {c.id}
+            image = {c.image}
+            name = {c.name}
+            birthday = {c.birthda}
+            gender = {c.gender}
+            job = {c.job}
+            />
+          ) ;
+        })
+      }
     </div>
   );
 }
