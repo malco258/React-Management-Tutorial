4b963b5846e08ba981c38c1a72fda640101e6544
dduck <kdh@rootechs.com>
2021-08-12 09:38:12 +0900

    Add  Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 import { classes } from 'istanbul-lib-coverage';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -19,25 +20,47 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
+
+/* Component Life Cycle
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount()
+*/
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    /*
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
+    */
   }
 
   callApi = async() => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render() {    
@@ -56,7 +79,13 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => { return (<Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} />) ;})
-            : ""}
+            :
+            <TableRow>
+              <TableCell colspan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
         
